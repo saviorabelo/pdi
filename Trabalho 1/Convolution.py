be89acca94e601329675fe182b3aaebaa0543f97
Savio <saviorabelo.ti@gmail.com>
2019-09-05 09:48:06 -0300

    Gaussian Kernel

--- Trabalho 1/Convolution.py
+++ Trabalho 1/Convolution.py
@@ -1,46 +1,64 @@
-import numpy as np
 import cv2
+import numpy as np
 
 class Convolution:
 
-    def __init__(self, image, kernel_size, kernel_type, times):
+    def __init__(self, image, kernel_size, kernel_type, iterations):
         self.image = image
         self.kernel = []
         self.kernel_size = kernel_size
         self.pad = int(kernel_size/2)
         self.kernel_type = kernel_type
-        self.times = times
+        self.iterations = iterations
         self.initKernel()
 
     def initKernel(self):
         if self.kernel_type == 'mean':
-            self.kernel = 1/9 * np.ones((self.kernel_size, self.kernel_size))
+            n = self.kernel_size
+            self.kernel = (1.0/(n*n)) * np.ones((n, n))
+        elif self.kernel_type == 'gaussian':
+            k = self.kernel_size
+            aux = cv2.getGaussianKernel(ksize=k,sigma=2)
+            self.kernel = aux @ aux.T
+        elif self.kernel_type == 'laplacian':
+            self.kernel = np.array(([0, 1, 0], 
+                                    [1, -4, 1], 
+                                    [0, 1, 0]), dtype='float')
+        elif self.kernel_type == 'sharpen':
+            self.kernel = np.array(([0, -1, 0], 
+                                    [-1, 5, -1], 
+                                    [0, -1, 0]), dtype='float')
+        elif self.kernel_type == 'sobelX':
+            self.kernel = np.array(([-1, 0, 1], 
+                                    [-2, 0, 2], 
+                                    [-1, 0, 1]), dtype='float')
+        elif self.kernel_type == 'sobelY':
+            self.kernel = np.array(([-1, -2, -1], 
+                                    [0, 0, 0], 
+                                    [1, 2, 1]), dtype='float')
+        else:
+            print('Erro, kernel type is not defined!\n')
 
     def convolve(self):
-
         (iH, iW) = self.image.shape
-        (kH, kW) = self.kernel.shape
         pad = self.pad
 
-        #image = np.zeros((pad+iH+pad, pad+iW+pad))
-        #image[pad:iH,pad:iW] = self.image
-        
-        #print(image.shape)
-
-        image = np.copy(self.image)
-        for _ in range(self.times):
-            image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+        image_aux = np.copy(self.image)
+        for _ in range(self.iterations):
+            # Making border for image (Padding)
+            image_border = np.zeros((pad+iH+pad, pad+iW+pad))
+            image_border[pad:-pad, pad:-pad] = image_aux
 
             output = np.zeros((iH, iW))
-            for y in np.arange(pad, iH + pad):
-                for x in np.arange(pad, iW + pad):
-                    roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+            for i in range(pad, iH + pad):
+                for j in range(pad, iW + pad):
+                    # Region of interest (roi)
+                    roi = image_border[i - pad:i + pad + 1, j - pad:j + pad + 1]
                     aux = (roi * self.kernel).sum()
-                    output[y - pad, x - pad] = np.floor(aux)
-            output = output.astype(np.uint64)
-            image = np.copy(output)
-        
-        
+                    output[i - pad, j - pad] = np.floor(aux)
+            output = np.uint8(output)
+            image_aux = np.copy(output)
+
         return output
     
     #def plotImage(self):
--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -5,9 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Convolution import Convolution
 
+# Load image
 image = cv2.imread('../Images/lena.png', cv2.IMREAD_GRAYSCALE)
 
-model = Convolution(image=image, kernel_size=3, kernel_type='mean', times=5)
+model = Convolution(image=image, kernel_size=5, kernel_type='mean', iterations=1)
 img_result = model.convolve()
 
 plt.imshow(img_result, 'gray')
