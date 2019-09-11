e2fd38ba2aedb4ead3497fb020e8e2da362280a2
Savio <saviorabelo.ti@gmail.com>
2019-09-11 18:31:38 -0300

    Results multi threshold

--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -4,8 +4,16 @@ import cv2
 from Operations import Operations
 
 # Load image
-image = cv2.imread('../Images/cameraman.png')
+path = '../Images/'
+name_file = 'cameraman.png'
+image = cv2.imread(path+name_file)
 
-model = Operations(image)
+model = Operations(image, name_file)
 image_result = model.convolve(kernel_size=3, kernel_type='mean', iterations=1)
 #model.otsu()
+#model.threshold(150)
+#model.multiThreshold(150, 170)
+
+
+
+#sorted(p)[4]
