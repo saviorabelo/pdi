1435ec309ccb66d11c96a9cac8dc3fa3acccda09
Savio <saviorabelo.ti@gmail.com>
2019-10-16 18:21:06 -0300

    Region Growth

--- Trabalho 2/Operations.py
+++ Trabalho 2/Operations.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from collections import deque
 from skimage import morphology
 import matplotlib.pyplot as plt
 from skimage.morphology import disk
@@ -14,13 +15,6 @@ class Operations:
             self.image = image
         self.name_file = name_file[:-4]
 
-    def thresholdMean(self):
-        media = np.mean(self.image)
-        img_bin = ((self.image >= media) * 255).astype('uint8')
-        
-        self.plotResult(img_bin, 'Binary')
-        return img_bin, media
-    
     def iterativeMean(self):
         img = self.image
         mean_initial = np.mean(img)
@@ -38,17 +32,87 @@ class Operations:
         self.plotResult(img_bin, 'Iterative Mean: {:.2f}'.format(media_new))
         #return img_bin, media_new
     
-    def watershed(self):
-        image = self.image
-        image_ext = morphology.dilation(image, disk(5)) - image
+    def plotResult(self, result, title):
+        fig = plt.figure(figsize=(8, 2.5), dpi=80)
+        a = fig.add_subplot(1,2,1)
+        a.axis('off')
+        plt.imshow(self.image, cmap=plt.get_cmap('gray'))
+        a.set_title('Original')
+
+        a = fig.add_subplot(1,2,2)
+        a.axis('off')
+        plt.imshow(result, cmap=plt.get_cmap('gray'))
+        a.set_title(title)
+
+        plt.show() 
+    
+    def getSeed(self, w, h):
+        p = 10.
         
-        m,n = image.shape
+        pos_ini_x_mrk = int(w/2 - p*w/100.)
+        pos_ini_y_mrk = int(h/2 - p*h/100.)
+        pos_fim_x_mrk = int(w/2 + p*w/100.)
+        pos_fim_y_mrk = int(h/2 + p*h/100.)
+
+        seed = np.zeros(shape=(w,h), dtype=np.uint8)
+        seed[pos_ini_x_mrk:pos_fim_x_mrk, pos_ini_y_mrk:pos_fim_y_mrk] = 255
+        
+        return seed
+
+    def neighbors(self, x, y, w, h):
+        points = [(x-1,y), (x+1, y), (x,y-1), (x,y+1),
+                (x-1,y+1), (x+1, y+1), (x-1,y-1), (x+1,y-1)]
+        
+        list_ = deque()
+        for p in points:
+            if (p[0]>=0 and p[1]>=0 and p[0]<w and p[1]<h):
+                list_.append(p)
+                
+        return list_
+        
+    def regionGrowth(self, epsilon=10):
+        image = cv2.blur(self.image, (5,5))
+        w, h = image.shape
+        
+        reg = self.getSeed(w, h)
+        queue = deque()
+        for x in range(w):
+            for y in range(h):
+                if reg[x, y] == 255:
+                    queue.append((x, y))
+        
+        while queue:
+            point = queue.popleft()
+            x = point[0]
+            y = point[1]
+
+            v_list = self.neighbors(x, y, w, h)
+            for v in v_list:
+                v_x = v[0]
+                v_y = v[1]
+                if((reg[v_x][v_y] != 255) and (abs(int(image[x][y])-int(image[v_x][v_y])) < epsilon)):
+                    reg[v_x][v_y] = 255
+                    queue.append(v)
+            
+        self.plotResult(reg, 'Region Growth')
+        #return reg
+
+    def getMarkers(self, m, n):
         markers = np.zeros([m,n])
         # Center
         m = int(m/2)
         n = int(n/2)
         markers[20:40,20:40] = 200
         markers[m:m+20,n:n+20] = 100
+
+        return markers
+
+    def watershed(self):
+        image = self.image
+        image_ext = morphology.dilation(image, disk(5)) - image
+        
+        m, n = image.shape
+        markers = self.getMarkers(m, n)
         
         ws = morphology.watershed(image_ext, markers)
         self.plotWatershed(image, 255-image_ext, markers, ws)
@@ -73,18 +137,4 @@ class Operations:
         ax3.axis('off')
 
         fig.tight_layout()
-        plt.show()
-
-    def plotResult(self, result, title):
-        fig = plt.figure(figsize=(9,3), dpi=80)
-        a = fig.add_subplot(1,2,1)
-        a.axis('off')
-        plt.imshow(self.image, cmap=plt.get_cmap('gray'))
-        a.set_title('Original')
-
-        a = fig.add_subplot(1,2,2)
-        a.axis('off')
-        plt.imshow(result, cmap=plt.get_cmap('gray'))
-        a.set_title(title)
-
         plt.show()
--- Trabalho 2/main.py
+++ Trabalho 2/main.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 # Import library
 import cv2
-import numpy as np
 from Operations import Operations
 
 # Load image
 path = '../Images/'
-name_file = 'leaf.jpg'
+name_file = '3303_lg.tiff'
 image = cv2.imread(path+name_file)
 
 model = Operations(image, name_file)
 #model.iterativeMean()
-model.watershed()
+#model.watershed()
+model.regionGrowth()
 
